c95360bc9da1390bbc7eab04168b31e8c3c196bb
DusanDjordjic <dukidjordjic@gmail.com>
2024-07-20 15:36:31 +0200

    save mp4 posts

--- scripts/download.py
+++ scripts/download.py
@@ -1,7 +1,7 @@
 import instaloader
 import os
 from PIL import Image
-
+from moviepy.editor import VideoFileClip
 
 images = []
 posts_file_path = 'posts'
@@ -33,7 +33,8 @@ def clean_and_rename(folder_path):
     print(files)
     # Initialize a counter for renaming jpg files
     jpg_count = 0
-
+    
+    files =  sorted(files, reverse=True)
     for file in files:
         if file.endswith('.jpg'):
             jpg_count += 1
@@ -53,7 +54,27 @@ def clean_and_rename(folder_path):
             img = f'<img src="./posts/{new_name}" />'
             images.append(img)
             print(f'Renamed {file} to {new_name}')
-        else:
+        elif file.endswith('.mp4'):
+            clip = VideoFileClip(file)
+
+            # Calculate new dimensions while maintaining aspect ratio
+            new_height = 500
+            ratio = new_height / clip.size[1]
+            new_width = int(clip.size[0] * ratio)
+
+            # Resize video
+            resized_clip = clip.resize(height=new_height, width=new_width)
+            
+            # Remove audio
+            final_clip = resized_clip.without_audio()
+
+            # Export the final clip as MP4 (or any other desired format)
+            jpg_count += 1
+            output_path = f'nails_by_dunja_post_{jpg_count}.mp4'
+            video = f' <video src="./posts/{output_path}" autoplay muted loop></video>'
+            images.append(video)
+            final_clip.write_videofile(output_path, codec='libx264', audio_codec='aac')
+        else :
             os.remove(file)
             print(f'Removed {file}')
 
